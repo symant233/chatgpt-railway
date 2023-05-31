142af26fd199cce377ce9979a204e78b1f10d29e
symant233 <symant233@gmail.com>
2023-05-31 14:25:45 +0800

    change avatar

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/25316350',
+      name: 'OpenAI',
+      description: 'Modified <a href="https://github.com/symant233/chatgpt-railway" class="text-blue-500" target="_blank" >repository</a>',
     },
   }
 }
